5df37b7dc1c637f005f9e0415e61c99c7ceeb7db
BASAN PATIL <47344640+basuofbgm@users.noreply.github.com>
2024-04-08 17:44:25 +1000

    Create bankAccount.cs
    S - Single Responsibility Principle (SRP)
    Ensure a class has only one responsibility and thus only one reason to change.

---
+++ bankAccount.cs
@@ -0,0 +1,10 @@
+public class BankAccount
+{
+    public void Deposit(decimal amount) { /*...*/ }
+    public void Withdraw(decimal amount) { /*...*/ }
+}
+
+public class BankAccountReport
+{
+    public void GenerateReport(BankAccount account) { /*...*/ }
+}
